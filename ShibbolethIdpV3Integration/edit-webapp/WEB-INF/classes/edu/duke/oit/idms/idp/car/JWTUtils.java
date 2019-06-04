a913d1e643af9a6edca29583b2b35e4681c7aea8
rob@duke.edu <rob@duke.edu>
2019-06-04 13:14:18 -0400

    Copyright and licensing

--- ShibbolethIdpV3Integration/edit-webapp/WEB-INF/classes/edu/duke/oit/idms/idp/car/JWTUtils.java
+++ ShibbolethIdpV3Integration/edit-webapp/WEB-INF/classes/edu/duke/oit/idms/idp/car/JWTUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 20xx - 20xx Duke University
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 2 as published by
+    the Free Software Foundation.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License Version 2
+    along with this program.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+ */
 package edu.duke.oit.idms.idp.car;
 
 import java.io.IOException;
