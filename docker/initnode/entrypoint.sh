f8ad2dccbb755f6d01d51467d4c15b2c1c8cea73
rob@duke.edu <rob@duke.edu>
2019-08-08 12:08:50 -0400

    Force content-type for initializing data stores

--- docker/initnode/entrypoint.sh
+++ docker/initnode/entrypoint.sh
@@ -32,24 +32,24 @@ curl -u "${CARMA_USER}:${CARMA_PASSWORD}" --insecure 'https://apache-sp/consent/
 # Force tomcat8 to deploy the informed content app
 # and load the demo "Amber" RH 
 #
-curl -u "${CARMA_USER}:$CARMA_PASSWORD" --insecure -X PUT -d @/tmp/demo_data/amber_rh_metainfo 'https://apache-sp/consent/v1/informed/rhic/entityId/urn:mace:multiverse:amber' >> /dev/null
+curl -u "${CARMA_USER}:$CARMA_PASSWORD" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_rh_metainfo 'https://apache-sp/consent/v1/informed/rhic/entityId/urn:mace:multiverse:amber' >> /dev/null
 
 #
 # Load attributes available through the Amber RH
 #
 for num in amberTitle cn displayName eduPersonOrcid eduPersonOrgDN eduPersonPrimaryAffiliation eduPersonPrincipalName eduPersonScopedAffiliation eduPersonTargetedID eduPersonUniqueId isMemberOf mail sn
 do
-  curl -u "${CARMA_USER}:$CARMA_PASSWORD" --insecure -X PUT -d @/tmp/demo_data/amber_${num}_iimetainfo 'https://apache-sp/consent/v1/informed/iiic/entityId/urn:mace:multiverse:amber/attribute/'$num >> /dev/null
+  curl -u "${CARMA_USER}:$CARMA_PASSWORD" -H 'Content-type: application/json;charset=UTF-8'  --insecure -X PUT -d @/tmp/demo_data/amber_${num}_iimetainfo 'https://apache-sp/consent/v1/informed/iiic/entityId/urn:mace:multiverse:amber/attribute/'$num >> /dev/null
 done
 
 #
 # Load up information about the Pattern in Amber RP
 #
-curl -u "${CARMA_USER}:${CARMA_PASSWORD}" --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_metainfo 'https://apache-sp/consent/v1/informed/rpic/metainformation/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8'  --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_metainfo 'https://apache-sp/consent/v1/informed/rpic/metainformation/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
 
-curl -u "${CARMA_USER}:${CARMA_PASSWORD}" --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_optionaliilist 'https://apache-sp/consent/v1/informed/rpic/optionaliilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8'  --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_optionaliilist 'https://apache-sp/consent/v1/informed/rpic/optionaliilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
 
-curl -u "${CARMA_USER}:${CARMA_PASSWORD}" --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8'  --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
 
 # 
 # Replace the entrypoint routine with one that only forces deployments
