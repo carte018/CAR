ba95ba242b01949ded0036f7563aa431a8faf2dc
rob@duke.edu <rob@duke.edu>
2019-08-13 17:08:53 -0400

    Add Rebma pattern and Chaos entities to demo data

--- docker/initnode/entrypoint.sh
+++ docker/initnode/entrypoint.sh
@@ -40,6 +40,13 @@ echo "Loading Amber RH information"
 curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_rh_metainfo 'https://apache-sp/consent/v1/informed/rhic/metainformation/entityId/urn:mace:multiverse:amber' 
 
 #
+# And add the Chaos RH
+#
+
+echo "Loading Chaos RH information"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/chaos_rh_metainfo 'https://apache-sp/consent/v1/informed/rhic/metainformation/entityId/urn:mace:multiverse:chaos'
+
+#
 # Load attributes available through the Amber RH
 #
 for num in amberTitle cn displayName eduPersonOrcid eduPersonOrgDN eduPersonPrimaryAffiliation eduPersonPrincipalName eduPersonScopedAffiliation eduPersonTargetedID eduPersonUniqueId isMemberOf mail sn
@@ -57,6 +64,23 @@ curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;cha
 echo "And the rplist"
 curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_rh_rplist 'https://apache-sp/consent/v1/informed/rhic/rplist/entityId/urn:mace:multiverse:amber'
 
+# Ditto for Chaos
+
+for num in chaosFamily cn displayName edupersonOrcid eduPersonOrgDN eduPersonPrimaryAffiliation eduPersonPrincipalName eduPersonScopedAffiliation eduPersonTargetedID eduPersonUnique Id isMemberOf mail sn
+do
+  echo "Inserting attribute $num"
+  curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/chaos_${num}_iimetainfo 'https://apache-sp/consent/v1/informed/iiic/iimetainformation/entityId/urn:mace:multiverse:chaos/attribute/'$num
+done
+
+echo "Adding rhiilist"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/chaos_rh_iilist 'https://apache-sp/consent/v1/informed/rhic/iilist/entityid/urn:mace:multiverse:chaos'
+
+echo "Adding rh infotypelist"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/chaos_rh_infotypes 'https://apache-sp/consent/v1/informed/rhic/infotypes/entityId/urn:mace:multiverse:chaos'
+
+echo "Adding rh rplist"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/chaos_rh_rplist 'https://apache-sp/consent/v1/informed/rhic/rplist/entityId/urn:mace:multiverse:chaos'
+
 #
 # Load up information about the Pattern in Amber RP
 #
@@ -69,6 +93,30 @@ curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;cha
 echo "Inserting Pattern in Amber RP required attributes"
 curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8'  --insecure -X PUT -d @/tmp/demo_data/amber_pattern_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.amber.org!shibboleth'
 
+#
+# And information about the Pattern in Rebma RP
+#
+echo "Inserting Pattern in Rebma RP metainfo"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/rebma_pattern_rp_metainfo 'https://apache-sp/consent/v1/informed/rpic/metainformation/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.rebma.org!shibboleth'
+
+echo "Inserting Pattern in Rebma RP optional attributes"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/rebma_pattern_rp_optionaliilist 'https://apache-sp/consent/v1/informed/rpic/optionaliilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.rebma.org!shibboleth'
+
+echo "Inserting Pattern in Rebma RP required attributes"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/rebma_pattern_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:!!pattern.rebma.org!shibboleth'
+
+#
+# And the Logrus (in the Courts of Chaos)
+#
+echo "Inserting Logrus metainfo"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/logrus_metainfo 'https://apache-sp/consent/v1/informed/rpic/metainformation/entityId/urn:mace:multiverse:chaos/entityId/https:!!logrus.coc.org!shibboleth'
+
+echo "Inserting Logrus optional attributes"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/logrus_optionaliilist 'https://apache-sp/consent/v1/informed/rpic/optionaliilist/entityId/urn:mace:multiverse:chaos/entityId/https:!!logrus.coc.org!shibboleth'
+
+echo "Inserting Logrus required attributes"
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/logrus_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:chaos/entityId/https:!!logrus.coc.org!shibboleth'
+ 
 # 
 # Replace the entrypoint routine with one that only forces deployments
 # -- on restart, we must not overwrite registration information.
