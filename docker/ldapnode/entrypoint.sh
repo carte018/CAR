4c541656c378123f606cd1ead33931b35ba1129b
rob@duke.edu <rob@duke.edu>
2019-08-18 14:46:21 -0400

    ldapnode adjustments

--- docker/ldapnode/entrypoint.sh
+++ docker/ldapnode/entrypoint.sh
@@ -6,12 +6,12 @@ $*
 # If the database isn't loaded, load it
 if [ ! -e /var/lib/ldaploaded ] 
 then
-	ldapadd -x -H ldap://127.0.0.1:7389 -D 'cn=admin,cn=config' -w 'config' -f /tmp/eduperson.schema
-	ldapadd -x -H ldap://127.0.0.1:7389 -D 'cn=admin,cn=config' -w 'config' -f /tmp/amberite.schema
-	ldapadd -x -H ldap://127.0.0.1:7389 -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/people.ldif
-	ldapadd -x -H ldap://127.0.0.1:7389 -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/idpuser.ldif
-	ldapmodify -x -H ldap://127.0.0.1:7389 -D 'cn=admin,cn=config' -w 'config' -f /tmp/idpuser.access
-	ldapadd -c -x -H ldap://127.0.0.1:7389 -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/users.ldif
+	ldapadd -x -H ldap://localhost -D 'cn=admin,cn=config' -w 'config' -f /tmp/eduperson.schema
+	ldapadd -x -H ldap://localhost -D 'cn=admin,cn=config' -w 'config' -f /tmp/amberite.schema
+	ldapadd -x -H ldap://localhost -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/people.ldif
+	ldapadd -x -H ldap://localhost -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/idpuser.ldif
+	ldapmodify -x -H ldap://localhost -D 'cn=admin,cn=config' -w 'config' -f /tmp/idpuser.access
+	ldapadd -c -x -H ldap://localhost -D 'cn=admin,dc=amber,dc=org' -w 'admin' -f /tmp/users.ldif
 
 	# Mark the database as loaded
 	touch /var/lib/ldaploaded
