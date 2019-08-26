2be665c2753e9fd2446c8c8a0cd2c782e999e7cb
rob@duke.edu <rob@duke.edu>
2019-08-26 17:03:02 -0400

    .

--- docker/idp/entrypoint.sh
+++ docker/idp/entrypoint.sh
@@ -3,7 +3,7 @@
 # Check for existing keys -- create new ones if they don't already exist
 #
 
-if [ ! -e /opt/shibboleth-idp/credentials/car_idp.key]
+if [ ! -e /opt/shibboleth-idp/credentials/car_idp.key ]
 then
     openssl req -newkey rsa:2048 -nodes -keyout /opt/shibboleth-idp/credentials/car_idp.key -x509 -days 365 -out /opt/shibboleth-idp/credentials/car_idp.crt
 fi
