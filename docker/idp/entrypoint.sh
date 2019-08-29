d9276abd0d0c11479a45de7415e07940495380b1
rob@duke.edu <rob@duke.edu>
2019-08-29 17:15:43 -0400

    .

--- docker/idp/entrypoint.sh
+++ docker/idp/entrypoint.sh
@@ -6,6 +6,7 @@
 if [ ! -e /opt/shibboleth-idp/credentials/car_idp.key ]
 then
     openssl req -newkey rsa:2048 -nodes -keyout /opt/shibboleth-idp/credentials/car_idp.key -x509 -days 365 -out /opt/shibboleth-idp/credentials/car_idp.crt -subj /C=US/ST=Colorado/L=Boulder/O=Internet2/OU=Skunkworks/CN=car-signing
+	cp /opt/shibboleth-idp/credentials /opt/shibboleth-idp/dist/
 fi
  
 exec $*
