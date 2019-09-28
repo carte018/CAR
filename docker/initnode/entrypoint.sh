3ca82f7d00e8f121fdfb32c290258cd98afe138c
rob@duke.edu <rob@duke.edu>
2019-09-28 15:33:19 -0400

    .

--- docker/initnode/entrypoint.sh
+++ docker/initnode/entrypoint.sh
@@ -183,7 +183,7 @@ echo "Inserting scholarly garage optional attributes"
 curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_scholars_rp_optionaliilist 'https://apache-sp/consent/v1/informed/rpic/optionaliilist/entityId/urn:mace:multiverse:amber/entityId/https:!!scholars.amber.org!shibboleth'
 
 echo "Inserting scholarly garage required attributes"
-curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_scholars_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:/!!scholars.amber.org!shibboleth'
+curl -u "${CARMA_USER}:${CARMA_PASSWORD}" -H 'Content-type: application/json;charset=UTF-8' --insecure -X PUT -d @/tmp/demo_data/amber_scholars_rp_requirediilist 'https://apache-sp/consent/v1/informed/rpic/requirediilist/entityId/urn:mace:multiverse:amber/entityId/https:!!scholars.amber.org!shibboleth'
 
 #
 # And apply the base policies for CARMA, which mandate release of everything to ourselves
