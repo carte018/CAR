56296f5a2feeb8a2d64dacfd3ef21a449552281d
rob@duke.edu <rob@duke.edu>
2020-02-09 11:04:05 -0500

    UTF8 i/o filtering in Tomcat

---
+++ copsu/src/main/java/edu/internet2/consent/copsu/util/CharSetFilter.java
@@ -0,0 +1,37 @@
+package edu.internet2.consent.copsu.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class CharSetFilter implements Filter {		 
+	 
+	    public void doFilter(
+	      ServletRequest request, 
+	      ServletResponse response, 
+	      FilterChain next) throws IOException, ServletException {
+	        request.setCharacterEncoding("UTF-8");
+	        response.setContentType("application/json; charset=UTF-8");
+	        response.setCharacterEncoding("UTF-8");
+	        if (next != null)
+	        	next.doFilter(request, response);
+	    }
+
+		@Override
+		public void init(FilterConfig filterConfig) throws ServletException {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void destroy() {
+			// TODO Auto-generated method stub
+			
+		}
+	 
+	
+}
